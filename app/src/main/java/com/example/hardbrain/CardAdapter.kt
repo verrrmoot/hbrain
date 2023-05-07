fad977bc1df383619f3760a78692986d1d7cbee3
verrrmoot <vera-0508@yandex.ru>
2023-05-07 09:56:47 +0300

    Вертит карточки в списке

--- app/src/main/java/com/example/hardbrain/CardAdapter.kt
+++ app/src/main/java/com/example/hardbrain/CardAdapter.kt
@@ -4,27 +4,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.ViewFlipper
 import androidx.recyclerview.widget.RecyclerView
 
-class CardAdapter(var cards: MutableList<Card>) : RecyclerView.Adapter<CardAdapter.ViewHolder>() {
+class CardAdapter(var cards: MutableList<Card>) : RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_card, parent, false)
-        return ViewHolder(view)
+        return CardViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun getItemCount(): Int = cards.size
+
+    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
         val card = cards[position]
-        holder.frontTextView.text = card.front
-        holder.backTextView.text = card.back
+        holder.bind(card)
     }
 
-    override fun getItemCount(): Int {
-        return cards.size
-    }
+    inner class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val tvFront: TextView = itemView.findViewById(R.id.tv_front)
+        private val tvBack: TextView = itemView.findViewById(R.id.tv_back)
+        private val viewFlipper: ViewFlipper = itemView.findViewById(R.id.view_flipper)
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val frontTextView: TextView = itemView.findViewById(R.id.frontTextView)
-        val backTextView: TextView = itemView.findViewById(R.id.backTextView)
+        init {
+            itemView.setOnClickListener {
+                viewFlipper.showNext()
+            }
+        }
+
+        fun bind(card: Card) {
+            tvFront.text = card.front
+            tvBack.text = card.back
+        }
     }
 }
